d879a0cc69fdce4977de8c64d35cf7f835d327ce
liuchengyuan <liucy@rp-pet.cn>
2020-11-03 14:14:07 +0800

    update

--- wepage-admin/src/App.vue
+++ wepage-admin/src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
+    <Hello></Hello>
     <keep-alive>
       <router-view v-if="$route.meta && $route.meta.keepAlive"></router-view>
     </keep-alive>
     <router-view v-if="!$route.meta || !$route.meta.keepAlive"></router-view>
   </div>
 </template>
-
+<script lang="ts">
+import Hello from "wepage-admin/components/HelloWorld.vue";
+export default {
+  components: {
+    Hello
+  }
+};
+</script>
 <style lang="scss"></style>
